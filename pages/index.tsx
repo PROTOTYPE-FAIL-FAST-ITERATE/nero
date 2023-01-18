34be093d5762886bd7f8835006a05949dedde3e5
Evan Lin <62809897+eevaain@users.noreply.github.com>
2023-01-17 23:25:45 -0500

    split about into 2 paragraphs

--- pages/index.tsx
+++ pages/index.tsx
@@ -103,7 +103,7 @@ const Home: NextPage = () => {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              nisi ut aliquip ex ea commodo consequat. <br/> <br/> Duis aute irure dolor in
               reprehenderit in voluptate velit esse cillum dolore eu fugiat
               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
               sunt in culpa qui officia deserunt mollit anim id est laborum
